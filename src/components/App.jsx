b00dba92e155945cb0e0728f16770d54c15d53d5
Maxim Noritsyn <maximnoritsyn@gmail.com>
2024-04-06 19:45:24 +0300

    friends

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,17 @@
 import user from '../data/user.json';
 import statistics from '../data/statistics.json';
+import friends from '../data/friends.json';
+
 import { Profile } from '../components/profile/profile';
 import { Statistics } from '../components/statistics/statistics';
+import { FriendList } from './friends/friends';
 
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
+        marginTop: 20,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -25,6 +29,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={statistics} />
       <Statistics stats={statistics} />
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/friends/friends.jsx
@@ -0,0 +1,16 @@
+import css from './friends.module.css'
+import { FriendListItem } from './friend/friend'
+
+export const FriendList = ({ friends }) => {
+    return <ul className={css.friendList}>
+        {friends.map(friend => (
+            <FriendListItem
+                key={friend.id}
+                avatar={friend.avatar}
+                name={friend.name}
+                isOnline={friend.isOnline}
+            />
+        ))}
+    </ul>
+}
+    
